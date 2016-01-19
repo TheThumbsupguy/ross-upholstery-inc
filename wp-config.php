2b19856996cfbfadf37f5c219ead9306801ffa68
rossupho <rossupho@a2ss19.a2hosting.com>
2016-01-19 01:58:46 -0500

    updating files to production, staging has been removed.

--- wp-config.php
+++ wp-config.php
@@ -20,10 +20,10 @@ define('WP_CACHE', true); // Added by W3 Total Cache
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'rossupho_stage');
+define('DB_NAME', 'rossupho_ross');
 
 /** MySQL database username */
-define('DB_USER', 'rossupho_stage');
+define('DB_USER', 'rossupho_ross');
 
 /** MySQL database password */
 define('DB_PASSWORD', '3.S5P8r6m[');
